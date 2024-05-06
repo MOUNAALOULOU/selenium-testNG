80817ee6dc83acd37d35b0b26f8c289e9816cfdf
MOUNAALOULOU <monaaloulou@yahoo.fr>
2024-05-07 00:30:03 +0200

    update project

--- src/test/java/com/todos/tests/AddTodoTest.java
+++ src/test/java/com/todos/tests/AddTodoTest.java
@@ -1,5 +1,24 @@
 package com.todos.tests;
 
-public class AddTodoTest {
+import java.io.IOException;
 
+import org.testng.annotations.Test;
+
+import com.todos.page.TodoPage;
+import com.todos.utils.Setup;
+
+public class AddTodoTest extends Setup {
+
+	public AddTodoTest() throws IOException {
+		super();
+	}
+
+	TodoPage todoPage;
+
+	@Test
+	public void iCanAddTodo() throws IOException {
+		todoPage = new TodoPage();
+		todoPage.submitTodo(prop.getProperty("todo1"));
+
+	}
 }
